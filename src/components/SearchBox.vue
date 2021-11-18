3a2067af0e9ab149643c4a15136f05d576a89ed9
xXSamXx00 <samudichio@gmail.com>
2021-11-18 13:48:58 +0100

    Esercizio completato

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,16 +1,32 @@
 <template>
     <div class="search text-center">
         <input v-model="filmSearch" type="search" placeholder="Cerca un Film">
-        <button class="btn btn-primary">Cerca</button>
+        <button class="btn btn-primary ms-4" @click="$emit('search_film', filmSearch)">Cerca</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            filmSearch: ""
+        }
+    }
 }
 </script>
 
 <style lang="scss">
-
+.search {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    input {
+        height: 38px;
+        width: 300px;
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 10px;
+    }
+}
 </style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,16 +1,44 @@
 <template>
     <header>
-        <SearchBox/>
+        <SearchBox :filmSearch="searchText" @search_film="searchFilm"/>
+        <ul class="movies">
+            <li class="movie" v-for="film in films" :key="film.id">
+                <p>{{ film.title }}</p>
+                <p>{{ film.original_title }}</p>
+                <p>{{ film.original_language }}</p>
+                <p>{{ film.vote_average }}</p>
+            </li>
+        </ul>
     </header>
 </template>
 
 <script>
 import SearchBox from './SearchBox.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    SearchBox
-  }
+    components: {
+        SearchBox
+    },
+    data() {
+        return {
+            films: [],
+            searchText: ""
+        }
+    },
+    methods: {
+        searchFilm(text) {
+            this.searchText = text
+            axios
+                .get(`https://api.themoviedb.org/3/search/movie?api_key=213b9fdb9078ddf64c88787abf1f0f84&language=it-IT&query=${this.searchText}&page=1&include_adult=true`)
+                .then(r => {
+                    this.films = r.data.results
+                }
+            ).catch(e => {
+                console.log(e, "DIO PORCO!");
+            })
+        }
+    }
 }
 </script>
 
