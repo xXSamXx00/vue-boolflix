eda5f13327f2a9431c9a09d6a2109a8dae72d4c7
xXSamXx00 <samudichio@gmail.com>
2021-11-19 13:19:20 +0100

    Mailstone 2 complete

---
+++ src/components/SerieElement.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="serie mb-3">
+        <p>{{ serieElement.name }}</p>
+        <p>{{ serieElement.original_name }}</p>
+        <p>{{ serieElement.vote_average }}</p>
+        <img height="30" class="mb-3" :src="require(`../assets/${serieElement.original_language}.png`)" alt="">
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        serieElement: Object
+    }
+}
+</script>
+
+<style lang="scss">
+.serie {
+    width: 400px;
+    background-color: cornflowerblue;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 16px 20px 0;
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,25 +1,43 @@
 <template>
-    <ul class="movies mt-4">
-        <FilmComponent v-for="film in films" :key="film.id" :filmElement="film"/>
-    </ul>
+    <div>
+        <h2 class="text-center">Film</h2>
+        <ul class="movies mt-4">
+            <FilmComponent v-for="film in films" :key="film.id" :filmElement="film"/>
+        </ul>
+        <h2 class="text-center">Serie TV</h2>
+        <ul class="series mt-4">
+            <SerieElement v-for="serie in series" :key="serie.id" :serieElement="serie"/>
+        </ul>
+    </div>
 </template>
 
 <script>
 import FilmComponent from './FilmComponent.vue'
+import SerieElement from './SerieElement.vue'
 
 export default {
     components: {
-        FilmComponent
+        FilmComponent,
+        SerieElement
     },
     props: {
         films: Array,
-        // series: Array
+        series: Array
     }
 }
 </script>
 
 <style lang="scss">
+h2 {
+    font-size: 20px;
+}
 .movies {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    list-style: none;  
+}
+.series {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-evenly;
